f5f95d3264f6230918d626caaad0bee6df070379
darkestdark4 <windcyborg@gmail.com>
2023-06-03 20:32:45 +0700

    repair filter, sort and searching

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react'
-import { getCountries, searchCountry, filterByRegion } from './api.js'
+import { getCountries } from './api.js'
 import CountryList from './components/CountryList.jsx'
 import { Listbox } from '@headlessui/react'
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline'
+
 const App = () => {
-  const continents = [
+  const filterData = [
     { name: 'All' },
     { name: 'Asia' },
     { name: 'Oceania' },
@@ -20,23 +21,18 @@ const App = () => {
     { id: 'small_area', name: 'Smallest area' },
     { id: 'big_area', name: 'Biggest area' }
   ]
-  
-  const [selectedContinent, setSelectedContinent] = useState([])
-  const [selectedSort, setSelectedSort] = useState([])
+
   const [countriesModify, setCountriesModify] = useState([])
   const [countries, setCountries] = useState([])
+  const [keyword, setKeyword] = useState('')
+  const [filter, setFilter] = useState(filterData[0])
+  const [sort, setSort] = useState(sortData[0])
 
   useEffect(() => {
-    getAllCountries()
-    setSelectedSort(sortData[0])
-    setSelectedContinent(continents[0])
+    resetData()
   }, [])
 
   const getAllCountries = () => {
-    setTimeout(() => {
-      console.log(selectedSort, selectedContinent)
-    }, 2000)
-    
     getCountries().then(result => {
       setCountries(result)
       setCountriesModify(result)
@@ -45,74 +41,98 @@ const App = () => {
     })
   }
 
-  const searchCountries = async (query) => {
-    const continent = selectedContinent.name
+  const searchCountries = (query) => {
+    let result = countries
 
-    if(query === '') {
-      resetQuery()
-    } else {
-      if(query.length > 3) {
-        let result = await searchCountry(query)
+    if(query.length > 3) {
+      result = countries.filter(country => {
+        // lower alphabet
+        const name = country.name.common.toLowerCase()
+        query = query.toLowerCase()
 
-        if(continent !== 'All') {
-          result = result.filter(item => {
-            return item.region === continent
-          })
-        }
-
-        setCountries(result)
-      }
+        return name.indexOf(query) > -1
+      })
     }
+
+    return result
   }
 
-  const filterByContinent = (val) => {
-    setSelectedContinent(val)
-    
+  const filterCountries = (data, val) => {
+    let result = []
+
     if(val.name === 'All') {
-      getAllCountries()
+      result = data
     } else {
-      const result = countries.filter(item => {
+      result = data.filter(item => {
         return item.region === val.name
       })
-      setCountriesModify(result)
     }
+
+    return result
   }
-  
-  const sortCountries = (val) => {
+
+  const sortCountries = (data, val) => {
     let result = []
-    setSelectedSort(val)
-    
+
     switch(val.id) {
       case 'low_pop':
-        result = countries.sort((a, b) => {
+        result = data.sort((a, b) => {
           return a.population - b.population
         })
         break;
       case 'high_pop':
-        result = countries.sort((a, b) => {
+        result = data.sort((a, b) => {
           return b.population - a.population
         })
         break;
       case 'small_area':
-        result = countries.sort((a, b) => {
+        result = data.sort((a, b) => {
           return a.area - b.area
         })
         break;
       case 'big_area':
-        result = countries.sort((a, b) => {
+        result = data.sort((a, b) => {
           return b.area - a.area
         })
         break;
       case 'no_sort':
-        getAllCountries()
+        result = data
         break;
     }
-    
-    if(val.id !== 'no_sort') {
-      setCountriesModify(result)
-    }
+
+    return result
   }
-  
+
+  const modifyCountries = (type, val) => {
+    let searchValue = keyword
+    let filterValue = filter
+    let sortValue = sort
+
+    if(type === 'search') {
+      setKeyword(val)
+      searchValue = val
+    } else if(type === 'filter') {
+      setFilter(val)
+      filterValue = val
+    } else if(type === 'sort') {
+      setSort(val)
+      sortValue = val
+    }
+
+    let searchResult = searchCountries(searchValue)
+    let filterResult = filterCountries(searchResult, filterValue)
+    let sortResult = sortCountries(filterResult, sortValue)
+
+    setCountriesModify(sortResult)
+  }
+
+  const resetData = () => {
+    getAllCountries()
+    setKeyword('')
+    setFilter(filterData[0])
+    setSort(sortData[0])
+  }
+
   return <div className="bg-gray-100">
     <h2 className="text-center font-bold text-4xl p-7">Country Stuff</h2>
 
@@ -124,29 +144,30 @@ const App = () => {
           <input type="text"
             className="w-full h-full py-2 pr-4 pl-12 text-sm text-gray-500 caret-gray-500 drop-shadow-lg focus:outline-0 focus:ring-1 focus:ring-gray-300"
             placeholder="Search"
-            onChange={({ target }) => searchCountries(target.value)}
+            value={keyword}
+            onChange={({ target }) => {
+              modifyCountries('search', target.value)
+            }}
           />
         </div>
         
         {/*Filter and sort*/}
         <div className="flex gap-2">
-          {/*Sort by area and populatiton*/}
+          {/*Sort by area and population*/}
           <div className="w-48">
-            <Listbox value={selectedSort}
-              onChange={(selected) => {
-                sortCountries(selected)
-              }}
+            <Listbox value={sort}
+              onChange={(selected) => modifyCountries('sort', selected)}
             >
               <div className="flex flex-col relative">
                 <Listbox.Button className="bg-white px-4 py-2 text-sm text-left drop-shadow-lg">
-                  {selectedSort.name}
+                  {sort.name}
                 </Listbox.Button>
                 <Listbox.Options className="bg-white text-sm absolute top-12 right-0 z-50 w-full drop-shadow-lg">
-                  {sortData.map((sort) => (
-                    <Listbox.Option key={sort.id} value={sort}
+                  {sortData.map((sortItem) => (
+                    <Listbox.Option key={sortItem.id} value={sortItem}
                       className="relative py-2 px-4 my-1 hover:bg-gray-200"
                     >
-                      {sort.name}
+                      {sortItem.name}
                     </Listbox.Option>
                   ))}
                 </Listbox.Options>
@@ -156,17 +177,15 @@ const App = () => {
           
           {/*Filter by region*/}
           <div className="w-40">
-            <Listbox value={selectedContinent}
-              onChange={(selected) => {
-                filterByContinent(selected)
-              }}
+            <Listbox value={filter}
+              onChange={(selected) => modifyCountries('filter', selected)}
             >
               <div className="flex flex-col relative">
                 <Listbox.Button className="bg-white px-4 py-2 text-sm text-left drop-shadow-lg">
-                  {selectedContinent.name}
+                  {filter.name}
                 </Listbox.Button>
                 <Listbox.Options className="bg-white text-sm absolute top-12 right-0 z-50 w-full drop-shadow-lg">
-                  {continents.map((continent) => (
+                  {filterData.map((continent) => (
                     <Listbox.Option key={continent.name} value={continent}
                       className="relative py-2 px-4 my-1 hover:bg-gray-200"
                     >
@@ -176,6 +195,12 @@ const App = () => {
                 </Listbox.Options>
               </div>
             </Listbox>
+          </div>
+
+          {/*Reset button*/}
+          <div>
+            <button className="px-4 py-2 border text-sm text-white bg-blue-400 hover:bg-blue-500 drop-shadow-lg"
+              onClick={(e) => resetData()}>Reset</button>
           </div>
         </div>
       </div>
